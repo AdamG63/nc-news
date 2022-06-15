854171722bae8a3a79d8903bfdff3f21d1871cd0
AdamG63 <adamghumra43@gmail.com>
2022-06-15 12:23:17 +0100

    Will handle errors

--- src/Components/Votes.js
+++ src/Components/Votes.js
@@ -5,15 +5,21 @@ import { useParams } from "react-router-dom";
 const Votes = ({ pre_votes }) => {
   const [votes, setVotes] = useState(pre_votes);
   const [clicked, setClicked] = useState(false);
+  const [err, setErr] = useState(null);
   const { article_id } = useParams();
 
   const handleclick = () => {
     setVotes((currVotes) => currVotes + 1);
     setClicked(true);
-    patchArticleById(article_id).catch(() => {
+    patchArticleById(article_id).catch((err) => {
       setVotes((currVotes) => currVotes - 1);
+      setErr("Something went wrong, please try again.");
     });
   };
+
+  if (err) {
+    return <p>{err}</p>;
+  }
 
   return (
     <>
